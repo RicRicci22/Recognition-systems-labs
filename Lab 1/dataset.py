eb0e9db8b1a425ec61bdf78158c9c76d8c1056af
Rickyworkstation <riccardo.ricci-1@unitn.it>
2023-03-22 15:33:30 +0100

    Changes to lab 1

--- Lab 1/dataset.py
+++ Lab 1/dataset.py
@@ -44,65 +44,62 @@ class cats_dogs_dataset(Dataset):
         return img
 
 
-if __name__ == "__main__":
+def get_datasets(
+    train_images_dir,
+    test_images_dir,
+    validation_split=0,
+    train_percentage=1,
+    transforms_train=None,
+    transforms_val_test=None,
+):
+    all_training_imgs = os.listdir(train_images_dir)
+    random.shuffle(all_training_imgs)
+    all_training_imgs = all_training_imgs[
+        : int(train_percentage * len(all_training_imgs))
+    ]  # DISCARD SOME IMAGES TO SPEED UP TRAINING
 
-    def get_datasets(
-        train_images_dir,
-        test_images_dir,
-        validation_split=0,
-        train_percentage=1,
-        transforms_train=None,
-        transforms_val_test=None,
-    ):
-        all_training_imgs = os.listdir(train_images_dir)
-        random.shuffle(all_training_imgs)
-        all_training_imgs = all_training_imgs[
-            : int(train_percentage * len(all_training_imgs))
-        ]  # DISCARD SOME IMAGES TO SPEED UP TRAINING
+    val_imgs = all_training_imgs[: int(validation_split * len(all_training_imgs))]
+    train_imgs = all_training_imgs[int(validation_split * len(all_training_imgs)) :]
+    test_imgs = os.listdir(test_images_dir)
 
-        val_imgs = all_training_imgs[: int(validation_split * len(all_training_imgs))]
-        train_imgs = all_training_imgs[int(validation_split * len(all_training_imgs)) :]
-        test_imgs = os.listdir(test_images_dir)
+    trainset = cats_dogs_dataset(
+        train_images_dir, train_imgs, transforms=transforms_train, train=True
+    )
+    valset = cats_dogs_dataset(
+        train_images_dir, val_imgs, transforms=transforms_val_test, train=True
+    )
+    testset = cats_dogs_dataset(
+        test_images_dir, test_imgs, transforms=transforms_val_test, train=False
+    )
 
-        trainset = cats_dogs_dataset(
-            train_images_dir, train_imgs, transforms=transforms_train, train=True
-        )
-        valset = cats_dogs_dataset(
-            train_images_dir, val_imgs, transforms=transforms_val_test, train=True
-        )
-        testset = cats_dogs_dataset(
-            test_images_dir, test_imgs, transforms=transforms_val_test, train=False
-        )
+    return trainset, valset, testset
 
-        return trainset, valset, testset
 
-    def get_loaders(
-        trainset, valset, testset, batch_size, num_workers=0, pin_memory=True
-    ):
-        trainloader = DataLoader(
-            trainset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            worker_init_fn=_init_fn,
-            persistent_workers=True,
-        )
-        valloader = DataLoader(
-            valset,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=0,
-            pin_memory=pin_memory,
-            worker_init_fn=_init_fn,
-        )
-        testloader = DataLoader(
-            testset,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=0,
-            pin_memory=pin_memory,
-            worker_init_fn=_init_fn,
-        )
+def get_loaders(trainset, valset, testset, batch_size, num_workers=0, pin_memory=True):
+    trainloader = DataLoader(
+        trainset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        worker_init_fn=_init_fn,
+        persistent_workers=True,
+    )
+    valloader = DataLoader(
+        valset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=pin_memory,
+        worker_init_fn=_init_fn,
+    )
+    testloader = DataLoader(
+        testset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=pin_memory,
+        worker_init_fn=_init_fn,
+    )
 
-        return trainloader, valloader, testloader
+    return trainloader, valloader, testloader
